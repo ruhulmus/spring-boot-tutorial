05932806504de6afb30184b297f56e5c62a3de46
ruhulmus <ruhulmus@gmail.com>
2022-03-14 10:26:02 +0600

    basic confirugration

--- spring-boot-rest-api/src/main/java/com/ruhulmus/SpringBootRestApiApplication.java
+++ spring-boot-rest-api/src/main/java/com/ruhulmus/SpringBootRestApiApplication.java
@@ -1,4 +1,4 @@
-package com.ruhulmus.springbootrestapi;
+package com.ruhulmus;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- spring-boot-rest-api/src/test/java/com/ruhulmus/SpringBootRestApiApplicationTests.java
+++ spring-boot-rest-api/src/test/java/com/ruhulmus/SpringBootRestApiApplicationTests.java
@@ -1,4 +1,4 @@
-package com.ruhulmus.springbootrestapi;
+package com.ruhulmus;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
