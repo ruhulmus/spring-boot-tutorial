cbfbcdbc47844ebe77df2c13199b435f6ea99df5
ruhulmus <ruhulmus@gmail.com>
2022-03-14 15:40:09 +0600

    update

--- spring-boot-rest-api/src/main/java/com/ruhulmus/controller/UserController.java
+++ spring-boot-rest-api/src/main/java/com/ruhulmus/controller/UserController.java
@@ -1,24 +1,19 @@
 package com.ruhulmus.controller;
 
+import com.ruhulmus.dto.user.request.User;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
-
 import javax.validation.Valid;
 
 @RestController
-//@RequestMapping(path = "/user", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+@RequestMapping(path = "/user", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
 public class UserController {
 
-    @GetMapping(value = "/add")
-    public String hello() {
-        return "Hello World !! Rest Controller is working ...";
+    @PostMapping(path = "/add")
+    public String add(@Valid @RequestBody User user) {
+        System.out.println("user");
+        return "test";
     }
-
-//    @PostMapping(path = "/add")
-//    public String add(@Valid @RequestBody User user) {
-//        System.out.println("user");
-//        return "test";
-//    }
 
 
 }
--- spring-boot-rest-api/src/main/java/com/ruhulmus/persistence/Test.java
+++
@@ -1,4 +0,0 @@
-package com.ruhulmus.persistence;
-
-public class Test {
-}
