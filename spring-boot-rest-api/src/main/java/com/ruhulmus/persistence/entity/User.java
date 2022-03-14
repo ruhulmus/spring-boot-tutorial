4f55f732314c0d9d4402ae4e80c3b6c1672e51cc
ruhulmus <ruhulmus@gmail.com>
2022-03-14 15:53:46 +0600

    update

--- spring-boot-rest-api/src/main/java/com/ruhulmus/persistence/entity/User.java
+++ spring-boot-rest-api/src/main/java/com/ruhulmus/persistence/entity/User.java
@@ -1,10 +1,21 @@
 package com.ruhulmus.persistence.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.math.BigDecimal;
+
 import lombok.Getter;
 import lombok.Setter;
+
 @Getter
 @Setter
+@Entity
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
     private String fullName;
     private String userName;
     private String emailAddress;
