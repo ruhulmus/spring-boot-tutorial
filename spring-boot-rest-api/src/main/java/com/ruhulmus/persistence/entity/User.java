4600ac7e835b26980ecc14f899450e0e99ad34ac
ruhulmus <ruhulmus@gmail.com>
2022-03-14 15:58:55 +0600

    update

--- spring-boot-rest-api/src/main/java/com/ruhulmus/persistence/entity/User.java
+++ spring-boot-rest-api/src/main/java/com/ruhulmus/persistence/entity/User.java
@@ -2,6 +2,7 @@ package com.ruhulmus.persistence.entity;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import java.math.BigDecimal;
 
